5dedb3db1c9c156d825437747fffbd2b4df48eac
Seth Troisi <sethtroisi@google.com>
2022-01-26 20:49:38 -0800

    Script to create directories

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ cd pminus1-server
 # git submodule later
 # wget for people who aren't me
 ln -s ../../misc-scripts/mersenne/status_report/prime95_status.py .
+ln -s ../../misc-scripts/mersenne/status_report/prime95_manage.py .
 
 # Download P-1 files from ???
 
@@ -25,11 +26,47 @@ FLASK_APP=app.py FLASK_ENV=development flask run
 
 A local server should now be running at http://localhost:5090
 
+### Layout
+
+I considered two directory structures
+* 2 digits of Hex (256 folders)
+   * Pros: Each folder is more uniform
+   * Cons: Zipping is harder
+* X00K, X.YM
+   * Pros: Content is spatical close for zipping
+   * Cons: More code, folders have thousands of items per
+
+Space requirement is roughly
+
+```python
+for i in range(10**6, 10**7+1, 10**6):
+    print(f"|{i:8}|{primesieve.count_primes(i):6}|{sum(p + 2048 for p in primesieve.primes(i))/8e9:5.1f}|")
+```
+
+|Max Prime|Primes|Space (GB)|
+|---|---|---|
+| 1000000| 78498|  4.7|
+| 2000000|148933| 17.9|
+| 3000000|216816| 39.1|
+| 4000000|283146| 68.1|
+| 5000000|348513|104.9|
+| 6000000|412849|149.2|
+| 7000000|476648|201.0|
+| 8000000|539777|260.2|
+| 9000000|602489|326.8|
+|10000000|664579|400.6|
+
+I've targetted 3,000,000 (216,816 residual files) as my target.
+
+Opting for better names I've gone with `0.01M`, `0.51M`, `2.12M` BUT I'll nest them twice `1M/1.12M/M1120001`
+
+
 ### TODO
 
 * [x] View records
   * [ ] Allow sorting
 * [x] Download files
+* [ ] Import script
 * [ ] Upload files
 * [ ] Reserve work
 * [ ] Sorted list of "best" work to do
---
+++ setup.py
@@ -0,0 +1,58 @@
+'''
+Copyright 2021 Seth Troisi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+
+import os
+import time
+
+
+# TODO these should maybe live in a config file, be arguments, or ...
+
+SERVE_DIR = "/home/five/Downloads/GIMPS/p95_partials/serving/"
+
+MAX_N = 3 * 10 ** 6
+
+
+def create_descriptive_directories(base, n):
+    print(f"Create descriptive (1M/1.23M) directories in {base!r}")
+    if not os.path.isdir(base):
+        print(f"Does not exist: {base!r}")
+        exit(1)
+
+    time.sleep(5)
+
+    os.chdir(base)
+
+    for extra_dir in ("uploads", "temp", "bad", "old", "extra"):
+        if not os.path.exists(extra_dir):
+            print(extra_dir)
+            os.mkdir(extra_dir)
+
+    # folder per 10000 -> max of 1229, "average" of ~700
+    for interval in range(0, n, 10000):
+        outer = f"{interval // 10 ** 6}M"
+        inner = f"{interval  / 10 ** 6:.2f}M"
+        if not os.path.exists(outer):
+            print(outer)
+            os.mkdir(outer)
+
+        inner_path = os.path.join(outer, inner)
+        if not os.path.exists(inner_path):
+            print(inner_path)
+            os.mkdir(inner_path)
+
+if __name__ == "__main__":
+    create_descriptive_directories(SERVE_DIR, MAX_N)
